8984187b8280c04b0c7527f9ea01b3e3628229c7
KeerthanaVanga <keerthanavangaa@gmail.com>
2026-03-03 18:47:35 +0530

    Extend maxAge for accessToken to one week

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -9,7 +9,7 @@ export const setAuthCookies = (
     httpOnly: true,
     secure: true,
     sameSite: "none",
-    maxAge: 15 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
   res.cookie("refreshToken", refreshToken, {
